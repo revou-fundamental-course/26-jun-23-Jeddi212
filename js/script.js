385619575cf46bbec77b3fe008f6908a53dacd02
Jeddi212 <jedediahf51@gmail.com>
2023-07-05 18:49:49 +0700

    🍌 :: js validation

--- js/script.js
+++ js/script.js
@@ -23,12 +23,72 @@ function sliderFunc() {
         + '<figcaption class="fade">"' + quotes[idx - 1] + '"</figcaption>';
 
     sliderTwo.innerHTML = '<figure class="user-picture">'
-    + '<img src="img/profile/' + profile_pic[idx - 1] + '" alt="gambar placeholder user" class="fade">'
-    + '</figure>'
-    + '<div class="user-info">'
-    + '<h4 class="user-name fade">' + names[idx - 1] + '</h4>'
-    + '<p class="profesi fade">' + profesi[idx - 1] + '</p>'
-    + '</div>'
+        + '<img src="img/profile/' + profile_pic[idx - 1] + '" alt="gambar placeholder user" class="fade">'
+        + '</figure>'
+        + '<div class="user-info">'
+        + '<h4 class="user-name fade">' + names[idx - 1] + '</h4>'
+        + '<p class="profesi fade">' + profesi[idx - 1] + '</p>'
+        + '</div>'
 }
 
 setInterval(sliderFunc, 4000);
+
+
+function validateForm() {
+    var fullName = document.contactUs.fullName.value;
+    var email = document.contactUs.email.value;
+    var phone = document.contactUs.phone.value;
+    var message = document.getElementById("message").value;
+    var interest = document.contactUs.interest.value;
+
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (fullName == "") {
+        alert("Please provide your name");
+        document.contactUs.fullName.focus();
+        return false;
+    }
+
+    if (email == "") {
+        alert("Please provide your email");
+        document.contactUs.email.focus();
+        return false;
+    } else {
+        if (!email.match(mailformat)) {
+            alert("Your email address is invalid");
+            document.contactUs.email.focus();
+            return false;
+        }
+    }
+
+    if (phone == "") {
+        alert("Please provide your phone number");
+        document.contactUs.phone.focus();
+        return false;
+    } else {
+        if (/^[0-9]+$/.test(phone) && (phone.length < 11 || phone.length > 12)) {
+            alert("Your phone number is invalid");
+            document.contactUs.phone.focus();
+            return false;
+        }
+    }
+
+    if (message == "") {
+        alert("Please provide some message");
+        document.contactUs.message.focus();
+        return false;
+    }
+
+    message = message.trim()
+
+    if (interest == "") {
+        alert("Please select your interest");
+        document.contactUs.interest.focus();
+        return false;
+    }
+
+    alert("Hello" + fullName + "\nYour email is " + email
+        + "\nYour number is " + phone + ".\nYou gave some message '" + message
+        + "'\nLastly you have interest in " + interest + " product.")
+    return true;
+}
